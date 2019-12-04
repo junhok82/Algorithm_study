6ee0ef1c1ac06e82de3010e824ce8b3b5fed3b5a
SekyuShin <doublesk0810@gmail.com>
2019-12-04 19:12:31 +0900

    UPDATE SSK_BOJ_1912

--- DP/BOJ_1912/SSK_1912.cpp
+++ DP/BOJ_1912/SSK_1912.cpp
@@ -17,6 +17,7 @@ int main() {
 	for (int i = 0; i < array_size; i++) {
 		scanf("%d", &origin[i]);
 	}
+	max_sum_value = origin[0];
 	for (int i = 0; i < array_size; i++) {
 		if (sum_value <= 0) {
 			sum_value = 0;
